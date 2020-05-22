3af612494693f40ec41206a3146107323e58cd22
Joshua Moore <morejoshua99@hotmail.com>
2020-05-22 17:55:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,6 +17,7 @@ end
 def using_delete(array , string)
   array.delete(string)
 end
-using_delete_at (array , intger)
-array.delete(intger)
+
+def using_delete_at (array , 2)
+  array.delete(2)
 end
