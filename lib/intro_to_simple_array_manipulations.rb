94431cebbfe4c1e7aa7406494cf25ba0e031c9f4
Joshua Moore <morejoshua99@hotmail.com>
2020-05-22 18:03:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,4 +22,4 @@ def using_delete_at (array , integer)
   array.delete_at (2)
 end
 
-#using flatten & compact will increase the ammount of memory (more memory = longer exec time)
+#using flatten & compact will increase exec time
